0cefd45c99cb7838bdcbc48b898151114f690bb0
RachelKoldenhoven <rachel@koldenfrozen.com>
2017-07-17 10:13:21 +0900

    draw board with pos nums

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,4 +1,5 @@
 // draw game board, initialize game
+// log top row, divider, mid row, divider, bottom row
 
 // place a mark, validate input
 
@@ -9,9 +10,15 @@
 // redraw board
 
 // start new game
+let boardPositions = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 const drawBoard = function () {
   console.log('Tic Tac Toe!');
+  console.log(boardPositions[0] + '  |  ' + boardPositions[1] + '  |  ' + boardPositions[2]);
+  console.log('-- | --- | --');
+  console.log(boardPositions[3] + '  |  ' + boardPositions[4] + '  |  ' + boardPositions[5]);
+  console.log('-- | --- | --');
+  console.log(boardPositions[6] + '  |  ' + boardPositions[7] + '  |  ' + boardPositions[8]);
 };
 
 
