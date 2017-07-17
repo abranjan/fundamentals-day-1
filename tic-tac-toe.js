0a5f75e6eb02913b08b428b42328be5372c532b5
RachelKoldenhoven <rachel@koldenfrozen.com>
2017-07-17 10:27:38 +0900

    make methods public, take turn, redraw board

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,3 +1,4 @@
+const game = function () {
 // draw game board, initialize game
 // log top row, divider, mid row, divider, bottom row
 
@@ -10,22 +11,29 @@
 // redraw board
 
 // start new game
-let boardPositions = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  let boardPositions = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-const drawBoard = function () {
-  console.log('Tic Tac Toe!');
-  console.log(boardPositions[0] + '  |  ' + boardPositions[1] + '  |  ' + boardPositions[2]);
-  console.log('-- | --- | --');
-  console.log(boardPositions[3] + '  |  ' + boardPositions[4] + '  |  ' + boardPositions[5]);
-  console.log('-- | --- | --');
-  console.log(boardPositions[6] + '  |  ' + boardPositions[7] + '  |  ' + boardPositions[8]);
+
+  return {
+
+    drawBoard : function () {
+      console.log('Tic Tac Toe!');
+      console.log(boardPositions[0] + '  |  ' + boardPositions[1] + '  |  ' + boardPositions[2]);
+      console.log('-- | --- | --');
+      console.log(boardPositions[3] + '  |  ' + boardPositions[4] + '  |  ' + boardPositions[5]);
+      console.log('-- | --- | --');
+      console.log(boardPositions[6] + '  |  ' + boardPositions[7] + '  |  ' + boardPositions[8]);
+    },
+
+    takeTurn : function(position, letter) {
+      boardPositions[position] = letter;
+      this.drawBoard();
+    }
+
+  }
+
 };
 
+const ticTacToe = game();
 
-
-
-
-
-
-
-drawBoard();
+ticTacToe.drawBoard();
