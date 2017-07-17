e5bc6aa5936658d2842af5f0986383b7aebcc0c7
RachelKoldenhoven <rachel@koldenfrozen.com>
2017-07-17 11:51:22 +0900

    refactor validate to helper func

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -13,13 +13,39 @@ const game = function () {
 // redraw board
 
 // start new game
+
   let boardPositions = [1, 2, 3, 4, 5, 6, 7, 8, 9];
   let initialize = true;
+
+  const validateInput = function (position, letter) {
+    if(!(position >= 1  && position <= 9)) {
+      console.log('Please enter a number between 1 and 9 for the position.');
+      return
+    }
+    if(typeof letter != 'string') {
+      console.log('Please enter a string for the letter, x or o.');
+      return
+    }
+    if(!initialize && boardPositions[position - 1] != ' ') {
+      console.log('That position is already taken.  Please choose another position');
+      return
+    }
+    letter = letter.toUpperCase();
+    if(!(letter == 'X' || letter == 'O')) {
+      console.log('Please choose only x or o');
+      return
+    } else {
+      boardPositions[position - 1] = letter;
+    }
+
+  };
 
   return {
 
     drawBoard : function () {
-      console.log('Tic Tac Toe!');
+      if(initialize) {
+        console.log('Tic Tac Toe!');
+      }
       console.log(boardPositions[0] + '  |  ' + boardPositions[1] + '  |  ' + boardPositions[2]);
       console.log('-- | --- | --');
       console.log(boardPositions[3] + '  |  ' + boardPositions[4] + '  |  ' + boardPositions[5]);
@@ -33,25 +59,7 @@ const game = function () {
     },
 
     takeTurn : function(position, letter) {
-      if(!(position >= 1  && position <= 9)) {
-        console.log('Please enter a number between 1 and 9 for the position.');
-        return
-      }
-      if(typeof letter != 'string') {
-        console.log('Please enter a string for the letter, x or o.');
-        return
-      }
-      if(!initialize && boardPositions[position - 1] != ' ') {
-        console.log('That position is already taken.  Please choose another position');
-        return
-      }
-      letter = letter.toUpperCase();
-      console.log(letter);
-      if(!(letter == 'X' || letter == 'O')) {
-        console.log('Please choose only x or o');
-        return
-      }
-      boardPositions[position - 1] = letter;
+      validateInput(position, letter);
       this.drawBoard();
     }
 
