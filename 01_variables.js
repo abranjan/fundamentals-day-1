02c926cf988329f26bcfc87840d326fa1f6158fb
Brent Gardner <bgardner@squarelabs.net>
2017-08-18 15:53:54 +0900

    extract variables

---
+++ 01_variables.js
@@ -0,0 +1,11 @@
+// ******* Variables *******
+
+// 1. Declare a variable called 'name' and assign it to a string of your name.  Use console.log to print it.
+
+// 2. Declare a variable called 'year' and assign it to a number. Use console.log() to print it.
+
+// 3. Declare a variable called 'snack' and do not assign it a value.  Log it.
+
+// 4. Assign a string value to 'snack' and log it again.  What changed?
+
+// 5. Log the length of 'snack'.
--- exercises_hw.js
+++ exercises_hw.js
@@ -4,17 +4,6 @@
 
 
 
-// *******Variables*******
-
-// 1. Declare a variable called 'name' and assign it to a string of your name.  Use console.log to print it.
-
-// 2. Declare a variable called 'year' and assign it to a number.
-
-// 3. Declare a variable called 'snack' and do not assign it a value.  Log it.
-
-// 4. Assign a value to 'snack' and log it again.  What changed?
-
-// 5. Log the length of 'snack'.
 
 // *******Operators and Expressions*******
 
